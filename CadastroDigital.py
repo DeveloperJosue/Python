37a269ec29a3878b4111eb6c52f8a1905b8cfe20
Josué da Silva Santos <64866112+DeveloperJosue@users.noreply.github.com>
2021-02-20 12:16:54 -0300

    update03.2

--- CadastroDigital.py
+++ CadastroDigital.py
@@ -8,7 +8,7 @@ nome = str(input('Qual é seu nome: '))
 time.sleep(0.5)
 
 idade = int(input(f'Ótimo {nome}, agora qual é sua idade: '))
-if idade <= 17:
+if idade <= 17:   #Idade
     print(f'''Infelizmente {nome} voce não pode continuar. 
     Pois nossas vagas são destinadas há pessoas maiores de 18 anos.
     Muito Obrigado pelo contato. ''')
@@ -27,8 +27,7 @@ time.sleep (1)
 area = int(input('Qual a area desejada:')) #Escolha da área. 
 time.sleep(1)
 
-    #Áreas de TI
-if area == 1: 
+if area == 1:   #Áreas de TI
     print(f'''Então {nome}, você solicitou as vagas na area de Tecnologia.
     As especificas são:
     1 - Engenherio de Software 
@@ -47,8 +46,7 @@ if area == 1:
     elif areati == '4':
         print(f'{nome} sua vaga foi cadastrada em Programador FullStack')      # 4
     
-    #Áreas de Mecânica
-if area == 2: 
+if area == 2:   #Áreas de Mecânica 
     print(f'''Então {nome}, você socilitou as vagas na área de Mecânica:
     1 - Torneiro
     2 - Mecânico de Manutenção
@@ -63,8 +61,7 @@ if area == 2:
     elif areamec == '3':
         print(f'{nome} sua vaga foi cadastrada em Soldador. ')                 # 3
 
-    #Áreas de Eletrica
-if area == 3: 
+if area == 3:   #Áreas de Eletrica   
     print(f'''Então {nome}, você solicitou as vagas na area de Eletrica:
     1 - Eletricista Industrial
     2 - Eletricista Predial
