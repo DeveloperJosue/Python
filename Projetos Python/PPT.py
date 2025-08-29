09a6aa0a08fcf25b1e314cffe45654af9718c0e5
Josué Santos <developerjosues@gmail.com>
2025-08-28 22:18:36 -0300

    Adicionando os contadores, mudança do time.sleep, mensagem final e espaços

--- Projetos Python/PPT.py
+++ Projetos Python/PPT.py
@@ -7,12 +7,11 @@ print('-='*10)
 time.sleep(1)
 
 nome = str(input('Nome do Jogador: '))
-time.sleep(1)
+time.sleep(0.5)
+
+contV, contE, contD  = 0, 0, 0   #Contadores de Vitórias, Empates e Derrotas
 
 while True:
-    contV = 0
-    contD = 0
-    contE = 0
     itens = ('Pedra', 'Papel', 'Tesoura')
     computador = randint(0, 2)
 
@@ -20,11 +19,11 @@ while True:
     [ 0 ] Pedra
     [ 1 ] Papel
     [ 2 ] Tesoura ''')
-    time.sleep(1)
+    time.sleep(0.5)
 
     jogador = int(input('Qual é sua Jogada: '))
 
-    if jogador < 0 or jogador > 2:
+    if jogador < 0 or jogador > 2 or jogador == '00':
         print('Jogada INVÁLIDA!! Tente Novamente. ')
         continue
 
@@ -38,56 +37,74 @@ while True:
     print('Pô')
     time.sleep(0.5)
 
-    print('-='*10)
+#Momento da jogada
+
+    print('-='*15)
     print(f'Computador Jogou {itens[computador]}')
     time.sleep(0.5)
     print(f'Você Jogou {itens[jogador]}')
-    print('-='*10)
+    print('-='*15)
 
-    if computador == 0:   #Computador Jogou Pedra
+#Computador Jogou Pedra
+
+    if computador == 0:   
         if jogador == 0:
             print('Empate!!')
-            contE =+ 1
+            contE += 1
         elif jogador == 1:
             print('Você Venceu!!')
-            contV =+ 1
+            contV += 1
         elif jogador == 2:
             print('Você Perdeu!!')
-            contD =+ 1
+            contD += 1
         else:
             print('Jogada INVÁLIDA!! Tente Novamente. ')
 
-    elif computador == 1:   #Computador Jogou Papel
+#Computador Jogou Papel
+
+    elif computador == 1:   
         if jogador == 0:
             print('Você Perdeu!!')
-            contD =+ 1
+            contD += 1
         elif jogador == 1:
             print('Empate!!')
-            contE =+ 1
+            contE += 1
         elif jogador == 2:
             print('Você Ganhou!!')
-            contV =+1
+            contV += 1
         else:
             print('Jogada INVÁLIDA!! Tente Novamente. ')
 
-    elif computador == 2:   #Computador Jogou Tesoura
+#Computador Jogou Tesoura
+
+    elif computador == 2:   
         if jogador == 0:
             print('Você Ganhou!!')
-            contV =+1
+            contV += 1
         elif jogador == 1:
             print('Você Perdeu!!')
-            contD =+ 1
+            contD += 1
         elif jogador == 2:
             print('Empate')
-            contE =+ 1
+            contE += 1
         else:
             print('Jogada INVÁLIDA!! Tente Novamente. ')
-    
+
+#Reinício do Jogo ou finalização 
+
     novamente = str(input('Quer jogar novamente[S/N]: ')).strip().upper() [0]
     if novamente == 'N':
         break 
-    time.sleep(1)
-print('-='*10)
-print(f'Você ganhou {contV} vezes, perdeu {contD} vezes e empatou {contE} vezes.')
-time.sleep(1)
+
+#Finalização do Jogo
+
+time.sleep(1) 
+print('-='*15)
+print(f'''Seu resumo da partida: 
+    Vitórias: {contV} vezes;
+    Derrotas: {contD} vezes;
+    Empates:  {contE} vezes.
+    Com o total de {contV + contD + contE} partidas.''') # Contagem Final
+print('-='*15)
+time.sleep(3)
 print(f'FIM DO JOGO! Obrigado por jogar {nome}.')
