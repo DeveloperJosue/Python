ba416491e58758117edd0b5a10afe719c570d556
Josué Santos <developerjosues@gmail.com>
2026-01-25 16:09:57 -0300

    Aprendendo Java

--- Projetos Java/project01/main.java
+++ Projetos Java/project01/main.java
@@ -1,6 +1,22 @@
+ import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ambiente Java configurado com sucesso!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual é seu nome: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
+        System.out.print("Qual sua idade? ");
+        int idade = scanner.nextInt();
+        System.out.println(name +  " tem " + idade + " anos.");
+        scanner.close();
+
+        if (idade >= 18) {
+            System.out.println("É maior de idade.");
+        } else {
+            System.out.println("É menor de idade.");
+        }
+
     }
 }
 
